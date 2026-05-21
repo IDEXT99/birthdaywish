1465ab56a9731059b50cd34d90395970eb7793d0
IDEXT99 <747dexter@gmail.com>
2026-05-21 21:09:13 +0530

    Update function.js

--- file/function.js
+++ file/function.js
@@ -5,6 +5,14 @@ var $win = $(window);
 var clientWidth = $win.width();
 var clientHeight = $win.height();
 
+$(window).resize(function() {
+    var newWidth = $win.width();
+    var newHeight = $win.height();
+    if (newWidth != clientWidth && newHeight != clientHeight) {
+        location.replace(location);
+    }
+});
+
 (function($) {
 	$.fn.typewriter = function() {
 		this.each(function() {
